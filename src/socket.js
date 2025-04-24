9a40ceee39a55a25ddcd6c45a3330975702dbd0b
DICE7574 <a4894894@gmail.com>
2025-04-24 15:34:39 +0900

    feat: disable auto-connect to allow manual socket control

--- src/socket.js
+++ src/socket.js
@@ -2,6 +2,6 @@ import { io } from 'socket.io-client';
 const SOCKET_SERVER_URL = 'http://localhost:3001';
 
 export const socket = io(SOCKET_SERVER_URL, {
-    autoConnect: true,
+    autoConnect: false,
     transports: ['websocket'],
 });
